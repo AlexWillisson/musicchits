7f0c2cd770d8a7a8abd6d042eb8d622de86158c5
Alex Willisson <alex.willisson@gmail.com>
2021-07-07 13:38:51 -0400

    detects purple and green squares

--- musicchits.py
+++ musicchits.py
@@ -11,25 +11,51 @@ while(1):
     _, frame = cap.read()
     # It converts the BGR color space of image to HSV color space
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-     
-    # Threshold of blue in HSV space
-    lower_green = np.array([40, 20, 30])
-    upper_green = np.array([70, 150, 200])
+
+    hues = {
+        "green": {
+            "lower": 40,
+            "upper": 70
+        },
+
+        "purple": {
+            "lower": 157,
+            "upper": 165
+        }
+    }
+
+    lower_saturation = 20
+    upper_saturation = 150
+
+    lower_value = 30
+    upper_value = 200
+
+    masks = []
+
+    for key in hues:
+        hue = hues[key]
+
+        lower = np.array([hue["lower"], lower_saturation, lower_value])
+        upper = np.array([hue["upper"], upper_saturation, upper_value])
+
+        mask = cv2.inRange(hsv, lower, upper)
+
+        masks.append(mask)
+
+    combined_masks = cv2.bitwise_or(masks[0], masks[1])
+
+    result = cv2.bitwise_and(frame, frame, mask = combined_masks)
  
-    # preparing the mask to overlay
-    mask = cv2.inRange(hsv, lower_green, upper_green)
-     
-    # The black region in the mask has the value of 0,
-    # so when multiplied with original image removes all non-green regions
-    result = cv2.bitwise_and(frame, frame, mask = mask)
- 
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(combined_masks, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
+
+        if w < 150 or h < 150:
+            continue
+
         cv2.rectangle(result, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     cv2.imshow('frame', frame)
-    # cv2.imshow('mask', mask)
     cv2.imshow('result', result)
      
     if cv2.waitKey(15) & 0xff == ord('q'):
