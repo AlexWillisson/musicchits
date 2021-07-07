d0b522b8bdb44d661ade55b39f5c26f065a99214
Alex Willisson <alex.willisson@gmail.com>
2021-07-07 13:17:34 -0400

    adds bounding box

--- musicchits.py
+++ musicchits.py
@@ -1,5 +1,7 @@
 #! /usr/local/bin/python3
 
+import sys
+
 import cv2
 import numpy as np
  
@@ -21,11 +23,16 @@ while(1):
     # so when multiplied with original image removes all non-green regions
     result = cv2.bitwise_and(frame, frame, mask = mask)
  
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(result, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
     cv2.imshow('frame', frame)
-    cv2.imshow('mask', mask)
+    # cv2.imshow('mask', mask)
     cv2.imshow('result', result)
      
-    if cv2.waitKey(20) & 0xff == ord('q'):
+    if cv2.waitKey(15) & 0xff == ord('q'):
         break
  
 cv2.destroyAllWindows()
